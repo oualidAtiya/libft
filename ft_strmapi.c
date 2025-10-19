fde7f74d67bbf4e8aeaf6b99e4ddaa22ad704664
oualidAtiya <oualidatiya@gmail.com>
2025-10-19 14:47:07 +0100

    ft_striteri and ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oatiya <oatiya@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 14:30:48 by oatiya            #+#    #+#             */
+/*   Updated: 2025/10/19 14:36:03 by oatiya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char    *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+    unsigned int    i;
+    char            *str;
+
+    if (!s)
+        return (NULL);
+    str = malloc(ft_strlen(s) + 1);
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (s[i])
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "libft.h" 
 #include <stdio.h>
+
+int add(int x, int y)
+{
+    return x + y ;
+}
 int main()
 {
-    printf("isprint function    => %d\n", ft_isprint('A'));
-    printf("ft_isalnum function => %d\n", ft_isalnum('A'));
-    printf("ft_isdigit function => %d\n", ft_isdigit('8'));
-    printf("ft_isalpha function => %d\n", ft_isalpha('A'));
-    printf("ft_isascii function => %d\n", ft_isascii('A'));
-
+   int (*fun)(int x, int y) ;
+   
+   fun = add ;
+   printf("%d",fun(3,5));
 }
