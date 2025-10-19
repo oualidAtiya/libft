160b24354e1cd5aa8119742ee5421a85aed1b262
oualidAtiya <oualidatiya@gmail.com>
2025-10-19 15:13:49 +0100

    finich *_fd functions

--- libft.h
+++ libft.h
@@ -1,7 +1,8 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <stdio.h>
 int ft_isalnum(char c);
 int ft_isalpha(char c);
 int ft_isdigit(char c);
@@ -9,7 +10,7 @@ int ft_isascii(int c);
 int ft_isprint(char c);
 int ft_atoi(const char *str);
 void ft_bzero(void *s, size_t n);
-void *ft_calloc(size_t count, size_t size);
+void    *ft_calloc(size_t count, size_t size);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
@@ -30,4 +31,10 @@ char *ft_strjoin(const char *s1, const char *s2);
 char *ft_strtrim(const char *s1, const char *set);
 char **ft_split(const char *s, char c);
 char *ft_itoa(int n);
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+char *ft_strmapi(const char *s, char (*f)(unsigned int, char));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 #endif
